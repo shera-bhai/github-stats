a909031cd22465025f8a9c662195fd0c7343b990
Shera Bhai <harsh.yadav1746@gmail.com>
2022-09-17 12:36:25 +0530

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -65,7 +65,7 @@ async def generate_languages(s: Stats) -> None:
     delay_between = 150
     for i, (lang, data) in enumerate(sorted_languages):
         color = data.get("color")
-        color = color if color is not None else "#FFFFFF"
+        color = "#000000"
         progress += (
             f'<span style="background-color: {color};'
             f'width: {data.get("prop", 0):0.3f}%;" '
